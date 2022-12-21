5b9d66167329968191715bc1feed0dd1c4531c55
Coolsnake8 <foxtailrb@gmail.com>
2022-12-20 20:38:59 -0700

    Solved puzzle1 of Day1.

--- Day1/puzzle1/Puzzle1.cs
+++ Day1/puzzle1/Puzzle1.cs
@@ -7,11 +7,29 @@ namespace puzzle1
     {
         static void Main(string[] args)
         {
-            FileStream fs = File.OpenRead(".\\inputData.txt");
+            string numbers = File.ReadAllText(".\\inputData.txt");
 
-            StreamReader sr = new StreamReader(fs);
+            string[] eachElf = numbers.Split("\n\n", StringSplitOptions.RemoveEmptyEntries);
 
-            Console.WriteLine(sr.ReadLine());
+            int[] arrayOfTotals = new int[eachElf.Length];
+
+            for (int i = 0 ; i < arrayOfTotals.Length ; i++)
+            {
+                string[] calories = eachElf[i].Split("\n", StringSplitOptions.RemoveEmptyEntries);
+
+                int total = 0;
+
+                foreach (string c in calories)
+                {
+                    total += int.Parse(c);
+                }
+
+                arrayOfTotals[i] = total;
+            }
+
+            Array.Sort(arrayOfTotals);
+
+            Console.WriteLine(arrayOfTotals[arrayOfTotals.Length - 1]);
         }
     }
 }
